543cbab30cd300c17968ed60381890f3312f3e68
gokul krish <gokloud5@gmail.com>
2023-07-21 23:30:35 +0530

    Datatype

--- Javascript/Practises/Basic/index.js
+++ Javascript/Practises/Basic/index.js
@@ -8,5 +8,31 @@ console.log(newVar);
 const emptyVar = null;
 console.log(emptyVar)
 
+
 let message;
-console.log(message)
+message = 'Hello';
+// alert(message);
+
+let str = "Hello"
+let str2 = 'Single quote'
+console.log(str, str2);
+
+let nameField = true;
+console.log(nameField); 
+
+let isGreate = 4 > 1;
+console.log(isGreate)
+
+let age=100;
+age = undefined;
+console.log(age);
+
+console.log(typeof undefined)
+console.log(typeof 0)
+console.log(typeof 10n)
+console.log(typeof true)
+console.log(typeof "foo")
+console.log(typeof Symbol("id"))
+console.log(typeof Math)
+console.log(typeof null)
+console.log(typeof alert)
