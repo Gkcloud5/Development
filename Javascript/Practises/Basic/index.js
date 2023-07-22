8fc77536f57daaddcd95fdb52560457f32e2400c
gokul krish <gokloud5@gmail.com>
2023-07-23 00:00:28 +0530

    js data type

--- Javascript/Practises/Basic/index.js
+++ Javascript/Practises/Basic/index.js
@@ -27,12 +27,42 @@ let age=100;
 age = undefined;
 console.log(age);
 
-console.log(typeof undefined)
-console.log(typeof 0)
-console.log(typeof 10n)
-console.log(typeof true)
-console.log(typeof "foo")
-console.log(typeof Symbol("id"))
-console.log(typeof Math)
-console.log(typeof null)
-console.log(typeof alert)
+console.log(typeof undefined);
+console.log(typeof 0);
+console.log(typeof 10n);
+console.log(typeof true);
+console.log(typeof "foo");
+console.log(typeof Symbol("id"));
+console.log(typeof Math);
+console.log(typeof null);
+console.log(typeof alert);
+
+let ages = prompt('How old are you?', 100);
+console.log(`You are ${ages} years old!`)
+// alert(`You are ${ages} years old!`);
+
+let confirm_test = confirm("are you okay?")
+console.log(confirm_test)
+
+let value1 = true;
+console.log(typeof value1);
+
+value2 = String(value1);
+console.log(typeof value2)
+
+let x = 1;
+
+x = -x;
+
+let x1 =1, y = 3;
+console.log(y - x1);
+
+console.log(5 + 7);
+console.log( 2 ** 2);
+console.log( 5 % 2);
+
+
+let s = "my" + "string";
+console.log(s);
+
+console.log(2 + 2 + '1');
